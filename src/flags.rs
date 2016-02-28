ef7877115b484045d5f09656c96cd3c632b9e221
Christopher Serr <cryze92@gmail.com>
2016-02-28 18:52:16 +0100

    Add a lot of Item IDs
    I couldn't find more without having to debug the game.

--- src/flags.rs
+++ src/flags.rs
@@ -4,7 +4,7 @@ pub use Addr;
 pub struct Flag(Addr, u8);
 
 pub const ANIMATION_SET_2: Flag = Flag(0x803B8759, 0x01);
-pub const MEDLI_ON_BOAT: Flag = Flag(0x803B8742, 0x28);
+pub const MEDLI_ON_BOAT: Flag = Flag(0x803B8742, 0x28); // FIXME 2 Flags
 pub const MEDLI_IN_EARTH_TEMPLE: Flag = Flag(0x803B8755, 0x20);
 pub const MEDLI_IN_EARTH_TEMPLE_ENTRANCE: Flag = Flag(0x803B875A, 0x04);
 
